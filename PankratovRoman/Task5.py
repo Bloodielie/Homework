f0d598d7ec924adef520ff7173000e9841c6843f
Bloodielie <riopro2812@gmail.com>
2021-09-30 17:35:54 +0300

    feat: add task-5 in session-5

---
+++ PankratovRoman/Task5.py
@@ -0,0 +1,35 @@
+from typing import Callable, TypeVar, Any
+
+T = TypeVar("T")
+
+
+def remember_result_factory() -> Callable[[Callable[..., T]], Callable[..., T]]:
+    last_result = None
+
+    def decorator(func: Callable[..., T]) -> Callable[..., T]:
+        def wrapper(*args: Any, **kwargs: Any) -> T:
+            nonlocal last_result
+            print(f"Last result = '{last_result}'")
+            last_result = func(*args, **kwargs)
+            return last_result
+
+        return wrapper
+
+    return decorator
+
+
+remember_result = remember_result_factory()
+
+
+@remember_result
+def sum_list(*args: str) -> str:
+    result = ""
+    for item in args:
+        result += item
+    print(f"Current result = '{result}'")
+    return result
+
+
+sum_list("a", "b")
+sum_list("abc", "cde")
+# sum_list(3, 4, 5)
